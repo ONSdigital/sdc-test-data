9ae50a7c7632fa10d2e4f195f9b230c210dd2b27
John Topley <john.topley@ons.gov.uk>
2017-07-11 09:33:08 +0100

    Collection exercise ID needs to match collection exercise seed data

--- case/install_test_data.sql
+++ case/install_test_data.sql
@@ -5,7 +5,7 @@ ALTER SEQUENCE casesvc.caseseq RESTART WITH 1;
 
 
 -- Case group
-INSERT INTO casesvc.casegroup(casegrouppk, id, partyid, collectionexerciseid, sampleunitref, sampleunittype) VALUES (nextval('casesvc.casegroupseq'), '9a5f2be5-f944-41f9-982c-3517cfcfef3c', '3b136c4b-7a14-4904-9e01-13364dd7b972', 'dab9db7f-3aa0-4866-be20-54d72ee185fb', '49900000000', 'B');
+INSERT INTO casesvc.casegroup(casegrouppk, id, partyid, collectionexerciseid, sampleunitref, sampleunittype) VALUES (nextval('casesvc.casegroupseq'), '9a5f2be5-f944-41f9-982c-3517cfcfef3c', '3b136c4b-7a14-4904-9e01-13364dd7b972', '14fb3e68-4dca-46db-bf49-04b84e07e77c', '49900000000', 'B');
 
 -- Reporting unit ('B') case
 INSERT INTO casesvc.case(casepk, id, casegroupfk, casegroupid, partyid, sampleunittype, collectioninstrumentid, state, actionplanid, createddatetime, createdby, iac) VALUES (nextval('casesvc.caseseq'), '7bc5d41b-0549-40b3-ba76-42f6d4cf3fdb', 1, '9a5f2be5-f944-41f9-982c-3517cfcfef3c', '3b136c4b-7a14-4904-9e01-13364dd7b972', 'B', '40c7c047-4fb3-4abe-926e-bf19fa2c0a1e', 'INACTIONABLE', 'e71002ac-3575-47eb-b87f-cd9db92bf9a7', current_timestamp, 'SYSTEM', 'fb747cq725lj');
