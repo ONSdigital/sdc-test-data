72ad8f7db06e0c3e007e6f9cbc34a9d1d717ff97
pricem14pc <pricem14pc@users.noreply.github.com>
2017-07-28 14:59:37 +0100

    Fixing json quote escape
    The original jsonb was throwing an error in the SQLAlchemy layer due to unescaped double quotes.

--- party/install_test_data.sql
+++ party/install_test_data.sql
@@ -1,5 +1,5 @@
 insert into ras_party.business (business_ref, party_uuid, attributes)
-	values (49900000000, '3b136c4b-7a14-4904-9e01-13364dd7b972', '{"contactName": "John Doe", "employeeCount": 50, "enterpriseName": "Bolts and Ratchets Ltd", "facsimile": "+44 1234 567890", "fulltimeCount": 35, "legalStatus": "Private Limited Company", "name": "Bolts and Ratchets Ltd", "sic2003": "2520", "sic2007": "2520", "telephone": "+44 1234 567890", "tradingName": "Bolts and Ratchets Ltd", "turnover": 350}');
+	values (49900000000, '3b136c4b-7a14-4904-9e01-13364dd7b972', '"{\"contactName\": \"John Doe\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
 
 insert into ras_party.respondent (party_uuid, email_address, first_name, last_name, telephone)
 	values ('db036fd7-ce17-40c2-a8fc-932e7c228397', 'jacky.turner@here.com', 'Jacky', 'Turner', '1234');
