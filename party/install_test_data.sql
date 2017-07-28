37f24a134a685e6dea6d0ffdb0e7a898053aa257
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-07-28 16:11:44 +0100

    a new sql file for party service

--- party/install_test_data.sql
+++ party/install_test_data.sql
@@ -1,5 +1,68 @@
-insert into ras_party.business (business_ref, party_uuid, attributes)
-	values (49900000000, '3b136c4b-7a14-4904-9e01-13364dd7b972', '"{\"contactName\": \"John Doe\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
+insert into business (business_ref, party_uuid, attributes)
+	values (49900000001, '3b136c4b-7a14-4904-9e01-13364dd7b973', '"{\"contactName\": \"Test User\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
 
-insert into ras_party.respondent (party_uuid, email_address, first_name, last_name, telephone)
-	values ('db036fd7-ce17-40c2-a8fc-932e7c228397', 'jacky.turner@here.com', 'Jacky', 'Turner', '1234');
+insert into business (business_ref, party_uuid, attributes)
+	values (49900000002, '3b136c4b-7a14-4904-9e01-13364dd7b974', '"{\"contactName\": \"Test User2\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited2\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
+
+insert into business (business_ref, party_uuid, attributes)
+	values (49900000003, '3b136c4b-7a14-4904-9e01-13364dd7b975', '"{\"contactName\": \"Test User3\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited3\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
+
+insert into business (business_ref, party_uuid, attributes)
+	values (49900000004, '3b136c4b-7a14-4904-9e01-13364dd7b976', '"{\"contactName\": \"Test User4\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited4\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
+
+insert into business (business_ref, party_uuid, attributes)
+	values (49900000005, '3b136c4b-7a14-4904-9e01-13364dd7b977', '"{\"contactName\": \"Test User5\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited5\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
+
+insert into business (business_ref, party_uuid, attributes)
+	values (49900000006, '3b136c4b-7a14-4904-9e01-13364dd7b978', '"{\"contactName\": \"Test User6\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited6\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
+
+insert into business (business_ref, party_uuid, attributes)
+	values (49900000007, '3b136c4b-7a14-4904-9e01-13364dd7b979', '"{\"contactName\": \"Test User7\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited7\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
+
+insert into business (business_ref, party_uuid, attributes)
+	values (49900000008, '3b136c4b-7a14-4904-9e01-13364dd7b980', '"{\"contactName\": \"Test User8\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited8\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
+
+insert into business (business_ref, party_uuid, attributes)
+	values (49900000009, '3b136c4b-7a14-4904-9e01-13364dd7b981', '"{\"contactName\": \"Test User9\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited9\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
+
+insert into business (business_ref, party_uuid, attributes)
+	values (49900000010, '3b136c4b-7a14-4904-9e01-13364dd7b982', '"{\"contactName\": \"Test User10\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited10\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
+
+insert into business (business_ref, party_uuid, attributes)
+	values (49900000011, '3b136c4b-7a14-4904-9e01-13364dd7b983', '"{\"contactName\": \"Test User11\", \"employeeCount\": 50, \"enterpriseName\": \"ABC Limited11\", \"facsimile\": \"+44 1234 567890\", \"fulltimeCount\": 35, \"legalStatus\": \"Private Limited Company\", \"name\": \"Bolts and Ratchets Ltd\", \"sic2003\": \"2520\", \"sic2007\": \"2520\", \"telephone\": \"+44 1234 567890\", \"tradingName\": \"ABC Trading Ltd\", \"turnover\": 350}"');
+
+
+
+insert into respondent (party_uuid, email_address, first_name, last_name, telephone)
+	values ('db036fd7-ce17-40c2-a8fc-932e7c228397', 'test.user@email.com', 'Test', 'User', '1234');
+
+insert into respondent (party_uuid, email_address, first_name, last_name, telephone)
+	values ('db036fd7-ce17-40c2-a8fc-932e7c228398', 'test.user2@email.com', 'Test', 'User2', '1234');
+
+insert into respondent (party_uuid, email_address, first_name, last_name, telephone)
+	values ('db036fd7-ce17-40c2-a8fc-932e7c228399', 'test.user3@email.com', 'Test', 'User3', '1234');
+
+insert into respondent (party_uuid, email_address, first_name, last_name, telephone)
+	values ('db036fd7-ce17-40c2-a8fc-932e7c228400', 'test.user4@email.com', 'Test', 'User4', '1234');
+
+insert into respondent (party_uuid, email_address, first_name, last_name, telephone)
+	values ('db036fd7-ce17-40c2-a8fc-932e7c228401', 'test.user5@email.com', 'Test', 'User5', '1234');
+
+insert into respondent (party_uuid, email_address, first_name, last_name, telephone)
+	values ('db036fd7-ce17-40c2-a8fc-932e7c228402', 'test.user6@email.com', 'Test', 'User6', '1234');
+
+insert into respondent (party_uuid, email_address, first_name, last_name, telephone)
+	values ('db036fd7-ce17-40c2-a8fc-932e7c228403', 'test.user7@email.com', 'Test', 'User7', '1234');
+
+insert into respondent (party_uuid, email_address, first_name, last_name, telephone)
+	values ('db036fd7-ce17-40c2-a8fc-932e7c228404', 'test.user8@email.com', 'Test', 'User8', '1234');
+
+insert into respondent (party_uuid, email_address, first_name, last_name, telephone)
+	values ('db036fd7-ce17-40c2-a8fc-932e7c228405', 'test.user9@email.com', 'Test', 'User9', '1234');
+
+insert into respondent (party_uuid, email_address, first_name, last_name, telephone)
+	values ('db036fd7-ce17-40c2-a8fc-932e7c228406', 'test.user10@email.com', 'Test', 'User10', '1234');
+
+
+
+
